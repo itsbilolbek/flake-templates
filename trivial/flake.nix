9f0134c02aa920665ad5fd41b6d3647348b60235
Normuminov Bilolbek <bilolbeknormuminov@gmail.com>
2026-01-29 17:26:45 +0500

    Add pre-commit, treefmt, flake-parts

--- trivial/flake.nix
+++ trivial/flake.nix
@@ -2,14 +2,72 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    git-hooks-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    inputs@{
+      # self,
+      # nixpkgs,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.git-hooks-nix.flakeModule
+      ];
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      perSystem =
+        {
+          config,
+          # self',
+          # inputs',
+          pkgs,
+          # system,
+          ...
+        }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              prettier.enable = true;
+            };
+          };
 
-  };
+          pre-commit = {
+            check.enable = true;
+            settings.hooks = {
+              treefmt.enable = true;
+              statix.enable = true;
+              deadnix.enable = true;
+            };
+          };
+
+          devShells.default = pkgs.mkShell {
+            name = "generic-dev-shell";
+
+            shellHook = config.pre-commit.installationScript;
+
+            nativeBuildInputs = with pkgs; [
+              just
+            ];
+          };
+        };
+    };
 }
