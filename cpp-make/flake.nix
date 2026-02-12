3c96078c156de68c0f0119efc8bb5c7dfbe86c4a
Bilolbek Normuminov <bilolbeknormuminov@gmail.com>
2026-02-12 13:58:43 +0500

    feat: add cpp-make template

---
+++ cpp-make/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "C hello world template using CMake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      imports = [
+        ./shell.nix
+      ];
+    };
+}
---
+++ cpp-make/src/main.cpp
@@ -0,0 +1,8 @@
+#include "mylib.h"
+#include <cstdio>
+
+int main() {
+  printf("Hello World from Main!\n");
+  print_hello();
+  return 0;
+}
